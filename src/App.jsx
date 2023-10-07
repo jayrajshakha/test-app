75e29aa99a59955e9fa9c54b0123399e6790108c
Jay Rajshakha <jayrajshakha01@gmail.com>
2023-10-07 11:42:19 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { useState } from "react"
 
- const App = () => {
+const App = () => {
 
-   const [a, setA] =  useState(0)
-
-   const handleClick = () => {
-     setA(a => a + 1)
-   }
   return (
     <div>
-      <h1>Hello World!</h1>
-      <h1>{a}</h1>
-      <button onClick={()=>handleClick()}>ok</button>
+        <h1>Jay Rajshakha </h1> 
     </div>
   )
 }
